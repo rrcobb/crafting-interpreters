056e1996acd9f1c5d5c9e195eae090360bb3df45
Robert Cobb <rwcobbjr@gmail.com>
2020-06-28 13:08:55 -0400

    add if statements

--- jlox-java/jlox/Parser.java
+++ jlox-java/jlox/Parser.java
@@ -16,10 +16,14 @@ declaration → varDecl
 varDecl → "var" IDENTIFIER ( "=" expression )? ";" ;
 
 statement   → exprStmt
-            | printStmt;
+            | ifStmt
+            | printStmt
+            | block;
 
 exprStmt  → expression ";" ;
+ifStmt    → "if" "(" expression ")" statement ( "else" statement )? ;
 printStmt → "print" expression ";" ;
+block → '{' statement* '}' ;
 
 expression → assignment ;
 assignment → IDENTIFIER "=" assignment
@@ -82,10 +86,25 @@ class Parser {
   }
 
   private Stmt statement() {
+    if (match(IF)) return ifStatement();
     if (match(PRINT)) return printStatement();
     if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
     return expressionStatement();
+  }
+
+  private Stmt ifStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'if'.");
+    Expr condition = expression();
+    consume(RIGHT_PAREN, "Expect ')' after if condition."); 
+
+    Stmt thenBranch = statement();
+    Stmt elseBranch = null;
+    if (match(ELSE)) {
+      elseBranch = statement();
+    }
+
+    return new Stmt.If(condition, thenBranch, elseBranch);
   }
 
   private Stmt printStatement() {
