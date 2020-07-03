054227248921732fd7ea2dc54f75a687fe22eb70
Robert Cobb <rwcobbjr@gmail.com>
2020-07-03 10:39:30 -0400

    finish ch 13, error on some invalid uses of super

--- jlox-java/jlox/Resolver.java
+++ jlox-java/jlox/Resolver.java
@@ -23,7 +23,8 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private enum ClassType {
     NONE,
-    CLASS
+    CLASS,
+    SUBCLASS
   }
 
   void resolve(List<Stmt> statements) {
@@ -113,6 +114,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     if (stmt.superclass != null) {
+      currentClass = ClassType.SUBCLASS;
       resolve(stmt.superclass);
     }
 
@@ -273,6 +275,14 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitSuperExpr(Expr.Super expr) {
+    if (currentClass == ClassType.NONE) {
+      Lox.error(expr.keyword,
+          "Cannot use 'super' outside of a class.");
+    } else if (currentClass != ClassType.SUBCLASS) {
+      Lox.error(expr.keyword,
+          "Cannot use 'super' in a class with no superclass.");
+    }
+
     resolveLocal(expr, expr.keyword);
     return null;
   }
