07145c60c0f29a9ab2c9b516983aa5cde8a1298a
Rob Cobb <rwcobbjr@gmail.com>
2024-09-09 14:28:08 -0600

    instances

--- clox-c/debug.c
+++ clox-c/debug.c
@@ -88,6 +88,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return byteInstruction("OP_SET_UPVALUE", chunk, offset);
     case OP_CONSTANT_LONG:
       return longConstantInstruction("OP_CONSTANT_LONG", chunk, offset);
+    case OP_GET_PROPERTY:
+      return constantInstruction("OP_GET_PROPERTY", chunk, offset);
+    case OP_SET_PROPERTY:
+      return constantInstruction("OP_SET_PROPERTY", chunk, offset);
     case OP_EQUAL:
       return simpleInstruction("OP_EQUAL", offset);
     case OP_GREATER:
